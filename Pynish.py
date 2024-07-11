5a066870b7ed27595ca4ea9e9c43048ec5ae07fe
KyblackNight <chessprodigio@gmail.com>
2024-07-11 16:06:28 -0300

    Pynish.py
    pkg install python3
    
    python3 Pynish.py

--- Pynish.py
+++ Pynish.py
@@ -53,31 +53,6 @@ option = input("--> ")
 print()
 
 if option == "1":
- time.sleep(1)
- print(Fore.YELLOW+"[==2%]")
- time.sleep(3)
- print(Fore.YELLOW+"[======25%]")
- time.sleep(2)
- print(Fore.YELLOW+"[===========60%]")
- time.sleep(5)
- print(Fore.YELLOW+"[100%]")
- os.system("clear")
- apres = (Fore.BLUE+"""
- ______                __         __
- |   __ \.--.--..-----.|__|.-----.|  |--.
- |    __/|  |  ||     ||  ||__ --||     |
- |___|   |___  ||__|__||__||_____||__|__|
-         |_____|
-
-
-       Developer:Dev Morais
-       Data: 9/Julho/2024
- """)
- print(apres)
- os.system("apt install hydra -y")
- print(Fore.GREEN+"Installation done successfully")
-else:
- if option =="2":
   time.sleep(1)
   print(Fore.YELLOW+"[==2%]")
   time.sleep(3)
@@ -87,60 +62,92 @@ else:
   time.sleep(5)
   print(Fore.YELLOW+"[100%]")
   os.system("clear")
-  print(apres)
-  os.system("termux-setup-storage")
-  os.system("pkg install wget -y")
-  os.system("wget -O install-nethunter-termux https://offs>
-  os.system("chmod +x install-nethunter-termux")
-  os.system("./install-nethunter-termux")
+  apres = (Fore.BLUE+"""
+  ______                __         __
+  |   __ \.--.--..-----.|__|.-----.|  |--.
+  |    __/|  |  ||     ||  ||__ --||     |
+  |___|   |___  ||__|__||__||_____||__|__|
+          |_____|
 
- else:
-  if option == "":
-   print(Fore.YELLOW+"Invalid !")
+
+        Developer:Dev Morais
+        Data: 9/Julho/2024
+  """)
+  print(apres)
+  os.system("apt install hydra -y")
+  print(Fore.GREEN+"Installation done successfully")
+else: 
+  if option =="2":
+    oi
+    time.sleep(1)
+    print(Fore.YELLOW+"[==2%]")
+    time.sleep(3)
+    print(Fore.YELLOW+"[======25%]")
+    time.sleep(2)
+    print(Fore.YELLOW+"[===========60%]")
+    time.sleep(5)
+    print(Fore.YELLOW+"[100%]")
+    os.system("clear")
+    print(apres)
+    os.system("termux-setup-storage")
+    os.system("pkg install wget -y")
+    os.system("wget -O install-nethunter-termux https://offs>
+    os.system("chmod +x install-nethunter-termux")
+    os.system("./install-nethunter-termux")
   else:
-   if option == "3":
-    exit()
-   else:
-     if option == "4":
-     time.sleep(1)
-     print(Fore.YELLOW+"[==2%]")
-     time.sleep(3)
-     print(Fore.YELLOW+"[======25%]")
-     time.sleep(2)
-     print(Fore.YELLOW+"[===========60%]")
-     time.sleep(5)
-     print(Fore.YELLOW+"[100%]")
-     os.system("clear")
-     print(apres)
-     os.system("sudo apt-get install perl")
-     os.system("git clone https://github.com/GHubgenius/sl>
-     os.system("cd slowloris.pl")
-     os.system("perl slowloris.pl")
-     print(Fore.GREEN+"Installation done successfully")
-    else:
-     if option == "5":
-      time.sleep(1)
-      print(Fore.YELLOW+"[==2%]")
-      time.sleep(3)
-      print(Fore.YELLOW+"[======25%]")
-      time.sleep(2)
-      print(Fore.YELLOW+"[===========60%]")
-      time.sleep(5)
-      print(Fore.YELLOW+"[100%]")
-      os.system("clear")
-     else:
-      time.sleep(1)
-      print(Fore.YELLOW+"[==2%]")
-      time.sleep(3)
-      print(Fore.YELLOW+"[======25%]")
-      time.sleep(2)
-      print(Fore.YELLOW+"[===========60%]")
-      time.sleep(5)
-      print(Fore.YELLOW+"[100%]")
-      os.system("clear")
-      print(apres)
-      os.system("git clone https://github.com/sqlmapprojec>
-      os.system("cd sqlmap")
-      os.system("apt install python")
-      os.system("python sqlmap.py")
+              
+              if option == "":
+                print(Fore.YELLOW+"Invalid !")
+              else:
+                if option == "3": 
+                  exit()
+                else:
+                  if option == "4":
+                    oi
+                    time.sleep(1)
+                    print(Fore.YELLOW+"[==2%]")
+                    time.sleep(3)
+                    print(Fore.YELLOW+"[======25%]")
+                    time.sleep(2)
+                    print(Fore.YELLOW+"[===========60%]")
+                    time.sleep(5)
+                    print(Fore.YELLOW+"[100%]")
+                    os.system("clear")
+                    print(apres)
+                    os.system("sudo apt-get install perl")
+                    os.system("git clone https://github.com/GHubgenius/sl>
+                    os.system("cd slowloris.pl")
+                    os.system("perl slowloris.pl")
+                    print(Fore.GREEN+"Installation done successfully")
+                  else:
+                              
+                              if option == "5":
+                                time.sleep(1)
+                                print(Fore.YELLOW+"[==2%]")
+                                time.sleep(3)
+                                print(Fore.YELLOW+"[======25%]")
+                                time.sleep(2)
+                                print(Fore.YELLOW+"[===========60%]")
+                                time.sleep(5)
+                                print(Fore.YELLOW+"[100%]")
+                                os.system("clear")
+                                print(apres)
+                                os.system("apt install nmap -y")
+                                print(Fore.GREEN+"Installation done successfully")
+                              else:
+                                
+                                time.sleep(1)
+                                print(Fore.YELLOW+"[==2%]")
+                                time.sleep(3)
+                                print(Fore.YELLOW+"[======25%]")
+                                time.sleep(2)
+                                print(Fore.YELLOW+"[===========60%]")
+                                time.sleep(5)
+                                print(Fore.YELLOW+"[100%]")
+                                os.system("clear")
+                                print(apres)
+                                os.system("git clone https://github.com/sqlmapprojec>
+                                os.system("cd sqlmap")
+                                os.system("apt install python")
+                                os.system("python sqlmap.py")
 
