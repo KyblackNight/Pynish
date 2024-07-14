dcc919778149407253642599f5f65204fa10fa47
KyblackNight <chessprodigio@gmail.com>
2024-07-13 21:30:06 -0300

    Pynish.py
    pkg install python3
    chmod +x *

--- Pynish.py
+++ Pynish.py
@@ -1,167 +1,213 @@
-#Devloper: Morais
-import time
+#Dev Morais 2.0
+import argparse as co
 import os
-os.system("clear")
-print("Loading...")
-time.sleep(2)
-os.system("apt install python3")
-os.system("apt install python3-pip -y")
-os.system("pip install colorama")
-
-
-
+import time
 import colorama
 from colorama import Fore, Back, Style
-os.system("clear")
-colorama.init(autoreset=True)
-
-
-apres = (Fore.BLUE+"""
-  ______                __         __
-  |   __ \.--.--..-----.|__|.-----.|  |--.
-  |    __/|  |  ||     ||  ||__ --||     |
-  |___|   |___  ||__|__||__||_____||__|__|
-          |_____|
-
-
-        Developer:Dev Morais
-        Data: 9/Julho/2024
-""")
-
-
-print(Fore.BLUE+"""
-
-  ______                __         __
- |   __ \.--.--..-----.|__|.-----.|  |--.
- |    __/|  |  ||     ||  ||__ --||     |
- |___|   |___  ||__|__||__||_____||__|__|
-         |_____|
-
-
-       Developer:Dev Morais
-       Data: 9/Julho/2024
-       Version: 1.0
-
-
-""")
-
-time.sleep(3)
-
-
-
-print(Fore.RED+"""
-
-[1] - Hydra Install
-[2] - Kali Termux Install
-[3] - Exit Pynish
-[4] - Slowloris Linux
-[5] - Nmap Termux
-[6] - Sqlmap Install
-
-
-""")
-
-time.sleep(2)
-
-option = input("--> ")
-print()
-
-if option == "1":
-  time.sleep(1)
-  print(Fore.YELLOW+"[==2%]")
-  time.sleep(3)
-  print(Fore.YELLOW+"[======25%]")
-  time.sleep(2)
-  print(Fore.YELLOW+"[===========60%]")
-  time.sleep(5)
-  print(Fore.YELLOW+"[100%]")
-  os.system("clear")
-  apres = (Fore.BLUE+"""
-  ______                __         __
-  |   __ \.--.--..-----.|__|.-----.|  |--.
-  |    __/|  |  ||     ||  ||__ --||     |
-  |___|   |___  ||__|__||__||_____||__|__|
-          |_____|
-
-
-        Developer:Dev Morais
-        Data: 9/Julho/2024
-  """)
-  print(apres)
-  os.system("apt install hydra -y")
-  print(Fore.GREEN+"Installation done successfully")
-else: 
-  if option =="2":
-    
-    time.sleep(1)
-    print(Fore.YELLOW+"[==2%]")
-    time.sleep(3)
-    print(Fore.YELLOW+"[======25%]")
-    time.sleep(2)
-    print(Fore.YELLOW+"[===========60%]")
-    time.sleep(5)
-    print(Fore.YELLOW+"[100%]")
+def pynish():
     os.system("clear")
-    print(apres)
-    os.system("termux-setup-storage")
-    os.system("pkg install wget -y")
-    os.system("wget -O install-nethunter-termux wget -O install-nethunter-termux https://offs.ec/2MceZWr")
-    os.system("chmod +x install-nethunter-termux")
-    os.system("./install-nethunter-termux")
-  else:
-              
-              if option == "":
-                print(Fore.YELLOW+"Invalid !")
-              else:
-                if option == "3": 
-                  exit()
-                else:
-                  if option == "4":
-                    
-                    time.sleep(1)
-                    print(Fore.YELLOW+"[==2%]")
-                    time.sleep(3)
-                    print(Fore.YELLOW+"[======25%]")
-                    time.sleep(2)
-                    print(Fore.YELLOW+"[===========60%]")
-                    time.sleep(5)
-                    print(Fore.YELLOW+"[100%]")
-                    os.system("clear")
-                    print(apres)
-                    os.system("sudo apt-get install perl -y")
-                    os.system("git clone https://github.com/GHubgenius/sl>")
-                    print(Fore.GREEN+"Installation done successfully")
-                  else:
-                              
-                              if option == "5":
-                                time.sleep(1)
-                                print(Fore.YELLOW+"[==2%]")
-                                time.sleep(3)
-                                print(Fore.YELLOW+"[======25%]")
-                                time.sleep(2)
-                                print(Fore.YELLOW+"[===========60%]")
-                                time.sleep(5)
-                                print(Fore.YELLOW+"[100%]")
-                                os.system("clear")
-                                print(apres)
-                                os.system("apt install nmap -y")
-                                print(Fore.GREEN+"Installation done successfully")
-                              else:
-                                if option == "6":
-                                  time.sleep(1)
-                                  print(Fore.YELLOW+"[==2%]")
-                                  time.sleep(3)
-                                  print(Fore.YELLOW+"[======25%]")
-                                  time.sleep(2)
-                                  print(Fore.YELLOW+"[===========60%]")
-                                  time.sleep(5)
-                                  print(Fore.YELLOW+"[100%]")
-                                  os.system("clear")
-                                  print(apres)
-                                  os.system("git clone https://github.com/sqlmapproject/sqlmap.git")
-                                  print(Fore.GREEN+"Installation done successfully")
-                                
-                                else:
-                                  print(Fore.YELLOW+"Invalid !")
-                                  
+    print("Loading...")
+    time.sleep(3)
+    os.system("pkg install python-pip")
+    os.system("pip install colorama")
 
+    os.system("clear")
+    colorama.init(autoreset=True)
+    apres = (Fore.BLUE + """
+      ______                __         __
+     |   __ \.--.--..-----.|__|.-----.|  |--.
+     |    __/|  |  ||     ||  ||__ --||     |
+     |___|   |___  ||__|__||__||_____||__|__|
+             |_____|
+
+           Developer: Dev Morais
+           Data: 9/Julho/2024
+           Version: 2.0
+       """)
+    print(apres)
+    print(Fore.RED + """
+    [1] - Hydra Install
+    [2] - Kali Termux Install
+    [3] - Exit Pynish
+    [4] - Slowloris Linux
+    [5] - Nmap Termux
+    [6] - Sqlmap Install
+    [7] - Metasploit Install
+    [8] - Dirb Install
+    """)
+    option = input("--> ")
+
+    if option == "1":
+        time.sleep(1)
+        print(Fore.YELLOW + "[==2%]")
+        time.sleep(3)
+        print(Fore.YELLOW + "[======25%]")
+        time.sleep(2)
+        print(Fore.YELLOW + "[===========60%]")
+        time.sleep(5)
+        print(Fore.YELLOW + "[100%]")
+        os.system("clear")
+        print(apres)
+        os.system("pkg install hydra -y")
+        print(Fore.GREEN + "Installation done successfully")
+    elif option == "2":
+        time.sleep(1)
+        print(Fore.YELLOW + "[==2%]")
+        time.sleep(3)
+        print(Fore.YELLOW + "[======25%]")
+        time.sleep(2)
+        print(Fore.YELLOW + "[===========60%]")
+        time.sleep(5)
+        print(Fore.YELLOW + "[100%]")
+        os.system("clear")
+        print(apres)
+        os.system("termux-setup-storage")
+        os.system("pkg install wget -y")
+        os.system("wget -O install-nethunter-termux https://offs.ec/2MceZWr")
+        os.system("chmod +x install-nethunter-termux")
+        os.system("./install-nethunter-termux")
+    elif option == "3":
+        exit()
+    elif option == "4":
+        time.sleep(1)
+        print(Fore.YELLOW + "[==2%]")
+        time.sleep(3)
+        print(Fore.YELLOW + "[======25%]")
+        time.sleep(2)
+        print(Fore.YELLOW + "[===========60%]")
+        time.sleep(5)
+        print(Fore.YELLOW + "[100%]")
+        os.system("clear")
+        print(apres)
+        os.system("sudo apt-get install perl")
+        os.system("git clone https://github.com/GHuom/GHubgenius/sl>")
+        print(Fore.GREEN + "Installation done successfully")
+    elif option == "5":
+        time.sleep(1)
+        print(Fore.YELLOW + "[==2%]")
+        time.sleep(3)
+        print(Fore.YELLOW + "[======25%]")
+        time.sleep(2)
+        print(Fore.YELLOW + "[===========60%]")
+        time.sleep(5)
+        print(Fore.YELLOW + "[100%]")
+        os.system("clear")
+        print(apres)
+        os.system("apt install nmap -y")
+        print(Fore.GREEN + "Installation done successfully")
+    elif option == "6":
+        time.sleep(1)
+        print(Fore.YELLOW + "[==2%]")
+        time.sleep(3)
+        print(Fore.YELLOW + "[======25%]")
+        time.sleep(2)
+        print(Fore.YELLOW + "[===========60%]")
+        time.sleep(5)
+        print(Fore.YELLOW + "[100%]")
+        os.system("clear")
+        print(apres)
+        os.system("git clone https://github.com/sqlmapproject/sqlmap.git")
+        print(Fore.GREEN + "Installation done successfully")
+    elif option == "7":
+      time.sleep(1)
+      print(Fore.YELLOW + "[==2%]")
+      time.sleep(3)
+      print(Fore.YELLOW + "[======25%]")
+      time.sleep(2)
+      print(Fore.YELLOW + "[===========60%]")
+      time.sleep(5)
+      print(Fore.YELLOW + "[100%]")
+      os.system("clear")
+      print(apres)
+      os.system("git clone https://github.com/gushmazuko/metasploit_in_termux.git")
+      print(Fore.GREEN+"Installation done successfully")
+    elif option == "8":
+      time.sleep(1)
+      print(Fore.YELLOW + "[==2%]")
+      time.sleep(3)
+      print(Fore.YELLOW + "[======25%]")
+      time.sleep(2)
+      print(Fore.YELLOW + "[===========60%]")
+      time.sleep(5)
+      print(Fore.YELLOW + "[100%]")
+      os.system("clear")
+      print(apres)
+      os.system("pkg install dirb")
+      print(Fore.GREEN+"Installation done successfully")
+      
+
+
+def main():
+    parser = co.ArgumentParser(description="Choose a number from 1 to 6 from the options")
+    parser.add_argument('-a', type=int, help="Number to be chosen from the options")
+    parser.add_argument('--list', metavar='STRING', type=str, nargs='?', const="Default message", help="Show list options")
+    args = parser.parse_args()
+
+    if args.list:
+      print(Fore.RED + """
+      [1] - Hydra Install
+      [2] - Kali Termux Install
+      [3] - Exit Pynish
+      [4] - Slowloris Linux
+      [5] - Nmap Termux
+      [6] - Sqlmap Install
+      [7] - Metasploit Install
+      [8] - Dirb Install
+      """)
+      exit()
+
+    if args.a:
+        print(f"{args.a}")
+        if args.a == 1:
+          os.system("pkg install hydra -y")
+          print(Fore.GREEN + "Installation done successfully")
+        else:
+          if args.a == 2:
+            os.system("termux-setup-storage")
+            os.system("pkg install wget -y")
+            os.system("wget -O install-nethunter-termux https://offs.ec/2MceZWr")
+            os.system("chmod +x install-nethunter-termux")
+            os.system("./install-nethunter-termux")
+          else:
+            if args.a == 3:
+              os.system("clear")
+              print("""
+              ####         ####
+              #  #         #  #
+              ###  ##  #   ###  ##  #
+              # ##  # #    # ##  # #
+              #  #  # #    #  #  # #
+              ####   #     ####   #
+                     #            #
+                     #            #
+
+
+              """)
+              exit()
+            else:
+              if args.a == 4:
+                os.system("sudo apt-get install perl")
+                os.system("git clone https://github.com/GHuom/GHubgenius/sl>")
+                print(Fore.GREEN + "Installation done successfully")
+              else:
+                if args.a == 5:
+                  os.system("apt install nmap -y")
+                  print(Fore.GREEN + "Installation done successfully")
+                else:
+                  if args.a == 6:
+                    os.system("git clone https://github.com/sqlmapproject/sqlmap.git")
+                    print(Fore.GREEN + "Installation done successfully")
+                  else:
+                    if args.a == 7:
+                      os.system("git clone https://github.com/gushmazuko/metasploit_in_termux.git")
+                      print(Fore.GREEN+"Installation done successfully")
+                    else:
+                      if args.a == 8:
+                        os.system("pkg install dirb")
+                        print(Fore.GREEN+"Installation done successfully")
+                        
+    else:
+        pynish()
+
+if __name__ == '__main__':
+    main()
